0ec139caa9ee4782cde8a509ecb3817f640a9ccd
mingshuo <1170709161@qq.com>
2025-05-12 10:33:14 +0800

    Spring Cloud Gateway引入

---
+++ auth/src/main/java/com/mingshuo/auth/AuthController.java
@@ -0,0 +1,18 @@
+package com.mingshuo.auth;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth/")
+public class AuthController {
+    @RequestMapping("doLogin")
+    public String doLogin(String username, String password) {
+        // 此处仅作模拟示例，真实项目需要从数据库中查询数据进行比对
+        if("zhang".equals(username) && "123456".equals(password)) {
+//            StpUtil.login(10001);
+            return "登录成功";
+        }
+        return "登录失败";
+    }
+}
